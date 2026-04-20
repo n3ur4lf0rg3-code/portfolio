4b1749a07f863a5b33eb926245e07920c0456047
n3ur4lf0rg3-code <n3ur4l.f0rg3@gmail.com>
2026-04-20 01:11:40 -0600

    Update App.jsx

--- projects/full-stack/pulse-ecommerce/frontend/src/App.jsx
+++ projects/full-stack/pulse-ecommerce/frontend/src/App.jsx
@@ -1,13 +1,19 @@
 import Products from "./modules/products/Products";
 import Cart from "./modules/cart/Cart";
 import Checkout from "./modules/cart/Checkout";
+import Login from "./modules/auth/Login";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 export default function App() {
   return (
     <>
+      <Login />
       <Products />
       <Cart />
-      <Checkout />
+
+      <ProtectedRoute>
+        <Checkout />
+      </ProtectedRoute>
     </>
   );
 }
