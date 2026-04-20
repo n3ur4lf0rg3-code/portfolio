21f9375a958dd8fb9294e5b8016acf85a25f7fd5
n3ur4lf0rg3-code <n3ur4l.f0rg3@gmail.com>
2026-04-20 00:33:28 -0600

    Create server.js

---
+++ projects/full-stack/pulse-ecommerce/backend/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import authRoutes from "./src/routes/auth.routes.js";
+import productRoutes from "./src/routes/product.routes.js";
+import orderRoutes from "./src/routes/order.routes.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/orders", orderRoutes);
+
+app.listen(3000, () => console.log("Server running on port 3000"));
