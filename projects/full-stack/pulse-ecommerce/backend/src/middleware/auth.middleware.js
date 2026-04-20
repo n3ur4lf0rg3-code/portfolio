7ca274c29769c9d025a5d166ace6f9ba91385ac1
n3ur4lf0rg3-code <n3ur4l.f0rg3@gmail.com>
2026-04-20 01:16:02 -0600

    Update auth.middleware.js

--- projects/full-stack/pulse-ecommerce/backend/src/middleware/auth.middleware.js
+++ projects/full-stack/pulse-ecommerce/backend/src/middleware/auth.middleware.js
@@ -7,7 +7,12 @@ export const authMiddleware = (req, res, next) => {
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded;
+
+    req.user = {
+      id: decoded.id,
+      role: decoded.role,
+    };
+
     next();
   } catch {
     res.status(403).json({ message: "Invalid token" });
