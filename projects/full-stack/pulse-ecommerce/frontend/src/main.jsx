0a70b334c8a8ea0ebed615790381e0d8c9f50a2b
n3ur4lf0rg3-code <n3ur4l.f0rg3@gmail.com>
2026-04-20 01:08:30 -0600

    Update main.jsx

--- projects/full-stack/pulse-ecommerce/frontend/src/main.jsx
+++ projects/full-stack/pulse-ecommerce/frontend/src/main.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { StoreProvider } from "./core/store";
+import { AuthProvider } from "./core/auth.context";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <StoreProvider>
-    <App />
-  </StoreProvider>
+  <AuthProvider>
+    <StoreProvider>
+      <App />
+    </StoreProvider>
+  </AuthProvider>
 );
