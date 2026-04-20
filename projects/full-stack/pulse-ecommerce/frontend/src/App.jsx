27fbb8a359da4b1dc02081395353a87b5212207f
n3ur4lf0rg3-code <n3ur4l.f0rg3@gmail.com>
2026-04-20 00:46:59 -0600

    Update App.jsx

--- projects/full-stack/pulse-ecommerce/frontend/src/App.jsx
+++ projects/full-stack/pulse-ecommerce/frontend/src/App.jsx
@@ -1,1 +1,13 @@
+import Products from "./modules/products/Products";
+import Cart from "./modules/cart/Cart";
+import Checkout from "./modules/cart/Checkout";
 
+export default function App() {
+  return (
+    <>
+      <Products />
+      <Cart />
+      <Checkout />
+    </>
+  );
+}
