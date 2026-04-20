10fe7d99b3b6770f239303418f224afbda78119a
n3ur4lf0rg3-code <n3ur4l.f0rg3@gmail.com>
2026-04-20 01:16:39 -0600

    Create role.middleware.js

---
+++ projects/full-stack/pulse-ecommerce/backend/src/middleware/role.middleware.js
@@ -0,0 +1,13 @@
+export const roleMiddleware = (roles = []) => {
+  return (req, res, next) => {
+    const userRole = req.user?.role;
+
+    if (!roles.includes(userRole)) {
+      return res.status(403).json({
+        message: "Forbidden: insufficient permissions",
+      });
+    }
+
+    next();
+  };
+};
