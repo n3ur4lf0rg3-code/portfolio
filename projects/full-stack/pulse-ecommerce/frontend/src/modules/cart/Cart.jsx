454f7a42fdb83340684d5789f74626ff8156f11f
n3ur4lf0rg3-code <n3ur4l.f0rg3@gmail.com>
2026-04-20 00:55:14 -0600

    Update Cart.jsx

--- projects/full-stack/pulse-ecommerce/frontend/src/modules/cart/Cart.jsx
+++ projects/full-stack/pulse-ecommerce/frontend/src/modules/cart/Cart.jsx
@@ -1,1 +1,14 @@
+import { useStore } from "../../core/store";
 
+export default function Cart() {
+  const { cart } = useStore();
+
+  return (
+    <div>
+      <h2>Cart</h2>
+      {cart.map((item, i) => (
+        <div key={i}>{item.name}</div>
+      ))}
+    </div>
+  );
+}
