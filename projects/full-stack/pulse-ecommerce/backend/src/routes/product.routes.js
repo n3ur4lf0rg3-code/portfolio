f73f2fe88f959ec900941ff9d303851b048ece87
n3ur4lf0rg3-code <n3ur4l.f0rg3@gmail.com>
2026-04-21 13:30:45 -0600

    Update product.routes.js

--- projects/full-stack/pulse-ecommerce/backend/src/routes/product.routes.js
+++ projects/full-stack/pulse-ecommerce/backend/src/routes/product.routes.js
@@ -2,6 +2,15 @@ import express from "express";
 import { getProducts, createProduct } from "../controllers/product.controller.js";
 import { authMiddleware } from "../middleware/auth.middleware.js";
 import { roleMiddleware } from "../middleware/role.middleware.js";
+import { validateProduct } from "../middleware/validateProduct.middleware.js";
+
+router.post(
+  "/",
+  authMiddleware,
+  roleMiddleware(["admin"]),
+  validateProduct,
+  createProduct
+);
 
 const router = express.Router();
 
