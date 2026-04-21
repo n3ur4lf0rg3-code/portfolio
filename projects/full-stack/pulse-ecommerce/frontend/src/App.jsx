b7b51dc8a6f2754722a03a09d2c72018378d5630
n3ur4lf0rg3-code <n3ur4l.f0rg3@gmail.com>
2026-04-21 12:52:27 -0600

    Add AdminPanel and AdminRoute to App component

--- projects/full-stack/pulse-ecommerce/frontend/src/App.jsx
+++ projects/full-stack/pulse-ecommerce/frontend/src/App.jsx
@@ -3,17 +3,17 @@ import Cart from "./modules/cart/Cart";
 import Checkout from "./modules/cart/Checkout";
 import Login from "./modules/auth/Login";
 import ProtectedRoute from "./components/ProtectedRoute";
+import AdminPanel from "./modules/admin/AdminPanel";
+import AdminRoute from "./components/AdminRoute";
 
 export default function App() {
   return (
     <>
-      <Login />
-      <Products />
-      <Cart />
+      {/* resto */}
 
-      <ProtectedRoute>
-        <Checkout />
-      </ProtectedRoute>
+      <AdminRoute>
+        <AdminPanel />
+      </AdminRoute>
     </>
   );
 }
