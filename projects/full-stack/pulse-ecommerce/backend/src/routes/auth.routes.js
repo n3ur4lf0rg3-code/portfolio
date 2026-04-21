1bee87ebda867d4a11bd3c573256caff7be3b99d
n3ur4lf0rg3-code <n3ur4l.f0rg3@gmail.com>
2026-04-21 13:25:01 -0600

    Update auth.routes.js

--- projects/full-stack/pulse-ecommerce/backend/src/routes/auth.routes.js
+++ projects/full-stack/pulse-ecommerce/backend/src/routes/auth.routes.js
@@ -1,5 +1,14 @@
 import express from "express";
 import { register, login } from "../controllers/auth.controller.js";
+import rateLimit from "express-rate-limit";
+
+const authLimiter = rateLimit({
+  windowMs: 10 * 60 * 1000,
+  max: 5, // solo 5 intentos
+  message: "Too many login attempts",
+});
+
+router.post("/login", authLimiter, login);
 
 const router = express.Router();
 
